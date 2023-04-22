d78b17bca3716073e26cb17f79fac5f79f32be20
Camilosm20 <madridcamilo606@gmail.com>
2023-04-22 11:52:41 -0500

    complete connection with sqli

--- database/MySQLi/Connection.php
+++ database/MySQLi/Connection.php
@@ -2,7 +2,20 @@
 
 $server = "localhost";
 $database = "finanzas_personales";
-$username = "root@localhost";
+$username = "root";
 $password = "";
+
+//Forma orientada a Objetos
+$mysqli = new mysqli($server, $username, "", $database);
+
+if ($mysqli->connect_errno) {
+    die("Fallo la conexion: {$mysqli->connect_error}");
+};
+
+//esto nos ayuda a poder usar cualquier caracter en nuestras consultas
+$setnames = $mysqli->prepare("SET NAMES utf8");
+$setnames->execute();
+
+var_dump($setnames);
 
 ?>
